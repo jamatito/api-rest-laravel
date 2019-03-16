d6fa29278fa060e5675d24c3f36030b2b2c92b86
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-03-16 21:11:31 +0100

    Terminado método de registrar usuarios con validaciones

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -31,7 +31,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -42,8 +42,8 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -54,11 +54,63 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
+    }
+
+    public function register(Request $request)
+    {
+        //  $json=$request->input()->all();
+        $json = $request->input('json', null);//es necesario poner como key 'json' en postman
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            $params_array = array_map('trim', $params_array);
+            //limpiamos los datos
+            $validate = \Validator::make($params_array, [
+                'name' => 'required|alpha',
+                'surname' => 'required|alpha',
+                'email' => 'required|email|unique:users',
+                //unique busca en la bd si ya existe uno igual
+                'password' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'El usuario no se ha creado',
+                    'errors' => $validate->errors()
+                );
+            } else {
+
+                User::create([
+                    'name' => $params_array['name'],
+                    'surname' => $params_array['surname'],
+                    'email' => $params_array['email'],
+                    'password' => password_hash($params_array['password'], PASSWORD_BCRYPT),
+                    'role' => 'ROLE_user'
+                ]);
+
+
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'El usuario se ha creado con exito'
+                );
+            }
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Los datos enviados no son correctos'
+            );
+
+        }
+        return response()->json($data, $data['code']);
     }
 }
