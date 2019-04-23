d8671b7be6f5d045a5652b65398cdb276f808824
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-04-23 23:35:09 +0200

    Creando modelo de comentario

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+
+    protected $fillable = [
+        'content', 'user_id', 'post_id', 'approved'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post', 'post_id');
+    }
 }
